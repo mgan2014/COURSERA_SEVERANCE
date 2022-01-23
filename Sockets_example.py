62ab99907cea1ae09a1bbf0adc0709a3dc819c02
mgan2014 <mason.gan@gmail.com>
2022-01-23 18:59:35 -0500

    fix the problem with \r in the cmd

--- Sockets_example.py
+++ Sockets_example.py
@@ -1,13 +1,18 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org',80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+# cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+# the above code won't work without \r for return sign
 mysock.send(cmd)
 
 while True:
     data = mysock.recv(512)
-    if (len(data) < 1):
+    if len(data) < 1:
         break
-    print(data.decode())
+    print(data.decode(),end='')
+
 mysock.close()
+
+# Code: http://www.py4e.com/code3/socket1.py
