5ede0f77638a4cb035ab0a43deccc900600b76ac
mgan2014 <mason.gan@gmail.com>
2022-01-31 20:46:37 -0500

    urllinks

---
+++ urllinks.py
@@ -0,0 +1,29 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+
+for i in range(0,7):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    j = 0
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    for tag in tags:
+        j += 1
+        if j == 18:
+            print(tag.get('href', None))
+            print(tag.contents[0])
+            url = tag.get('href', None)
+            break
