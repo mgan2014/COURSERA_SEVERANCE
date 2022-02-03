beb832580f704926b7436c7d6a74b0aaebced734
mgan2014 <mason.gan@gmail.com>
2022-02-03 16:46:40 -0500

    google geojson api not working due to public key

---
+++ geojson.py
@@ -0,0 +1,31 @@
+import json
+import urllib.request, urllib.parse,urllib.error
+
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+
+while True:
+    address = input('Enter location:')
+    if (len(address) < 1):
+        break
+
+    url = serviceurl + urllib.parse.urlencode({'address': address})
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url)
+    data = uh.read().decode()
+    print('Retrieved', len(data),'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('Failed to retrieve')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent = 4))
+    lat = js["results"][0]["geometry"]["location"]["lat"]
+    print('lat', lat)
+    location =js["results"][0]["formatted_address"]
+    print('location:', location)
